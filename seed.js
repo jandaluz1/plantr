bf4f967e472a216eb9e8c281ebf0d20fbba7e8da
Joe Andaluz <jandaluz1@gmail.com>
2018-06-18 17:32:16 -0400

    veg

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
-const db = require('./models');
+const { db, Vegetable, Plot, Gardener } = require('./models');
 
-db.sync()
+db.sync({ force: true })
   .then(() => {
     console.log('db sync successful');
     db.close();
@@ -9,3 +9,68 @@ db.sync()
     console.log('Error:', err);
     db.close();
   });
+
+// Vegetable.build({
+//   name: 'carrot',
+//   color: 'orange',
+//   planted_on: new Date()
+// })
+//   .save()
+//   .then(veg => {
+//     console.log(veg.name + ' Added.');
+//     db.close();
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// Vegetable.build({
+//   name: 'corn',
+//   color: 'yellow',
+//   planted_on: new Date()
+// })
+//   .save()
+//   .then(veg => {
+//     console.log(veg.name + ' Added.');
+//     db.close();
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+// Vegetable.build({
+//   name: 'lettuce',
+//   color: 'green',
+//   planted_on: new Date()
+// })
+//   .save()
+//   .then(veg => {
+//     console.log(veg.name + ' Added.');
+//     db.close();
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// Vegetable.bulkCreate([
+//   {
+//     name: 'carrot',
+//     color: 'orange',
+//     planted_on: new Date()
+//   },
+//   {
+//     name: 'corn',
+//     color: 'yellow',
+//     planted_on: new Date()
+//   },
+//   {
+//     name: 'lettuce',
+//     color: 'green',
+//     planted_on: new Date()
+//   }
+// ])
+//   .then(() => {
+//     db.close();
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
